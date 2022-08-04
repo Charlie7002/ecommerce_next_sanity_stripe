1c42fac739ec80a1743b76be04d850470a517ee8
charlie7002 <chaced@orange.fr>
2022-08-04 17:53:22 +0200

    fix

--- components/index.js
+++ components/index.js
@@ -1,6 +1,6 @@
-export { default as Footer } from './footer';
-export { default as HeroBanner } from './heroBanner';
-export { default as Product } from './product';
+export { default as Footer } from './Footer';
+export { default as HeroBanner } from './HeroBanner';
+export { default as Product } from './Product';
 export { default as FooterBanner } from './FooterBanner';
 export { default as Cart } from './Cart';
 export { default as Navbar } from './Navbar';
